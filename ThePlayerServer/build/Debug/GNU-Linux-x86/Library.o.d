ad122b2a27889c7a1271100ca862da451783d6e6
helcl42 <lubos.helcl@gmail.com>
2012-06-27 01:14:53 +0200

    beta

--- ThePlayerServer/build/Debug/GNU-Linux-x86/Library.o.d
+++
@@ -1,8 +0,0 @@
-build/Debug/GNU-Linux-x86/Library.o: Library.cpp Library.h Utils.h \
- Types.h
-
-Library.h:
-
-Utils.h:
-
-Types.h:
--- ThePlayerServer/build/Debug/GNU-Linux-x86/Logger.o.d
+++
@@ -1,3 +0,0 @@
-build/Debug/GNU-Linux-x86/Logger.o: Logger.cpp Logger.h
-
-Logger.h:
